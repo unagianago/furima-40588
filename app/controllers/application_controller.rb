96fe3027de7b608897de5b9dfa5e6c03865e1edb
unagianago <fumiyakm4580@gmail.com>
2024-05-16 18:51:26 +0900

    追加カラムのパラメーター設定とバリデーション設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
 
 private
@@ -8,6 +9,10 @@ private
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_sub, :last_name_sub, :birthday])
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+  validates :first_name_sub, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :last_name_sub, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :birthday, presence: true
+
 end
