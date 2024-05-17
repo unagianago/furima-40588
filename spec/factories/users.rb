48c7f901043789934e2814cda43b1ae92be57dc1
unagianago <fumiyakm4580@gmail.com>
2024-05-17 17:30:24 +0900

    passwordのFakerを半角英数字混合にした

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname { Faker::Name.initials(number: 2) }
     email { Faker::Internet.email }
-    password { Faker::Internet.password(min_length: 6) }
+    password { Faker::Internet.password(min_length: 6) + '1a'}
     password_confirmation { password }
     first_name { '横山' }
     last_name { '文哉' }
