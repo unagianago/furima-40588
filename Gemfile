dd2d0808ebc93b3ed8463072c79c1c18d987391d
unagianago <fumiyakm4580@gmail.com>
2024-05-20 11:11:43 +0900

    Active Storageの導入

--- Gemfile
+++ Gemfile
@@ -85,3 +85,5 @@ end
 
 gem 'devise'
 gem 'pry-rails'
+gem 'mini_magick'
+gem 'image_processing', '~> 1.2'
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,3 +2,9 @@ class ItemsController < ApplicationController
   def index
   end
 end
+
+private
+
+  def item_params
+    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+  end
