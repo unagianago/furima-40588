14e7b26c28c80a7b0d1f7085a5f5c83eb5d23b4d
unagianago <fumiyakm4580@gmail.com>
2024-05-20 17:24:26 +0900

    販売手数料と販売利益の表示

--- app/javascript/item_transaction.js
+++ app/javascript/item_transaction.js
@@ -1,3 +1,10 @@
-window.addEventListener('turbo:load', () => {
-  console.log("OK");
-});
+const transactionInput = document.getElementById("item-price");
+transactionInput.addEventListener("input", () => {
+  const inputValue = transactionInput.value;
+
+  const addTaxFee = document.getElementById("add-tax-price");
+  addTaxFee.innerHTML = Math.floor(inputValue*0.1)
+
+  const transactionProfit = document.getElementById("profit");
+  transactionProfit.innerHTML = Math.floor(inputValue-addTaxFee.innerHTML)
+})
