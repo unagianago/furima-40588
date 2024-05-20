8a771d7b27bdcd6d8a13f15385e42e4c2b9e12a2
unagianago <fumiyakm4580@gmail.com>
2024-05-20 14:33:38 +0900

    ルーティング及びコントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,18 @@
 class ItemsController < ApplicationController
+
+  before_action :authenticate_user!, only: [:new]
+
   def index
   end
+
+  def new
+    if user_signed_in?
+      @item = Items.new
+    else
+      redirect_to new_user_session_path
+    end
+  end
+  
 end
 
 private
