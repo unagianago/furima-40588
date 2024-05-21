c731893abfe34d8477adf2010c52dd2073ba07bb
unagianago <fumiyakm4580@gmail.com>
2024-05-21 19:51:49 +0900

    商品一覧を表示するためのビューファイルの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
 
   def index
-    @item = Item.includes(:user)
+    @items = Item.includes(:user).order(created_at: :desc)
   end
 
   def new
