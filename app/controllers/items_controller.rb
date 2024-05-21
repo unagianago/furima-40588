ec8a5a3f75623e3fe5f92e1613571e5e0c261c7c
unagianago <fumiyakm4580@gmail.com>
2024-05-21 16:13:20 +0900

    単体テストコードの追記

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,11 +5,7 @@ class ItemsController < ApplicationController
   end
 
   def new
-    if user_signed_in?
       @item = Item.new
-    else
-      redirect_to new_user_session_path
-    end
   end
 
   def create
