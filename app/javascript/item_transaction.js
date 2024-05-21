09275b174182b66fe4304ede09f034972d9c31ee
unagianago <fumiyakm4580@gmail.com>
2024-05-21 15:34:13 +0900

    Javascriptにturboの記述を追加

--- app/javascript/item_transaction.js
+++ app/javascript/item_transaction.js
@@ -1,10 +1,12 @@
-const transactionInput = document.getElementById("item-price");
-transactionInput.addEventListener("input", () => {
-  const inputValue = transactionInput.value;
+window.addEventListener('turbo:load', function(){
+  const transactionInput = document.getElementById("item-price");
+  transactionInput.addEventListener("input", () => {
+    const inputValue = transactionInput.value;
 
-  const addTaxFee = document.getElementById("add-tax-price");
-  addTaxFee.innerHTML = Math.floor(inputValue*0.1)
+    const addTaxFee = document.getElementById("add-tax-price");
+    addTaxFee.innerHTML = Math.floor(inputValue*0.1)
 
-  const transactionProfit = document.getElementById("profit");
-  transactionProfit.innerHTML = Math.floor(inputValue-addTaxFee.innerHTML)
+    const transactionProfit = document.getElementById("profit");
+    transactionProfit.innerHTML = Math.floor(inputValue-addTaxFee.innerHTML)
+  })
 })
