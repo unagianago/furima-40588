5d210de7eec5727a4d17b0add559f6b33d151c9a
unagianago <fumiyakm4580@gmail.com>
2024-05-22 16:40:18 +0900

    ルーティング、コントローラー及びビューの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new,:edit]
 
   def index
     @items = Item.includes(:user).order(created_at: :desc)
@@ -21,6 +21,25 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
+
+  def edit
+    @item = Item.find(params[:id])
+    unless  current_user == @item.user
+      redirect_to root_path
+    end
+  end
+
+  def update
+    item = Item.find(params[:id])
+
+    if item.update(item_params)
+      redirect_to item_path(item.id)
+    else
+      @item = item
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
 end
 
 private
