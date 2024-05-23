58233d6e2eddba643ada77371bfab6ebae038ac5
unagianago <fumiyakm4580@gmail.com>
2024-05-24 08:45:40 +0900

    rubocop によるコード修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,6 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index]
-  before_action :item_information, only: [:index,:create]
+  before_action :item_information, only: [:index, :create]
   def index
     gon.public_key = ENV['PAYJP_PUBLIC_KEY']
     @order_address = OrderAddress.new
