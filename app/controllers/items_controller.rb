2a56a6f403eb14d3fb07bbd5f13dad70709fc844
unagianago <fumiyakm4580@gmail.com>
2024-05-23 21:17:25 +0900

    他機能関連

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,7 +23,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    return if current_user == @item.user
+    return if current_user == @item.user && @item.order == nil
 
     redirect_to root_path
   end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,12 @@
 class OrdersController < ApplicationController
-  
+  before_action :authenticate_user!, only: [:index]
   def index
     gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @order_address = OrderAddress.new
     item_information
+    if (current_user != @item.user && @item.order != nil) || (current_user == @item.user) 
+    redirect_to root_path
+    end
   end
 
   def create
