b0506bcf60b21dc5af4080efd02db0618365569b
unagianago <fumiyakm4580@gmail.com>
2024-05-23 09:52:23 +0900

    createアクションの定義

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,22 @@
 class OrdersController < ApplicationController
   
   def index
-    @item = Item.find(params[:id])
     @order_address = OrderAddress.new
+  end
+
+  def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def order_params
+    params.require(:order_address).permit(:post_code,:town,:house_number,:building,:phone_number,:area_id).merge(item_id:params[:item_id],user_id: current_user.id)
   end
 
 end
